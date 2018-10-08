f0a68ffe0b594608c7803ef40edb445f546fee90
Amol Deshmukh <34318357+des137@users.noreply.github.com>
2018-10-07 21:53:52 -0400

    Function generalized for an array of matrices.

--- create_submission_csv.py
+++ create_submission_csv.py
@@ -8,38 +8,45 @@ $ ls -1 > list.txt
 
 import csv 
 
-def format_converter(input_matrix):
+def format_converter(input_matrix_set):
     """
-    Converts a matrix into a string of the locations and counts of 1's: the format required by kaggle.
+    Converts an array of matrices into an array of strings of the locations and counts of 1's: 
+    i.e. the format required by kaggle.
     For e.g.
-    >>> sample = np.array([[1, 0, 1], [1, 1, 1], [0, 1, 1]])
+    >>> sample = np.array([[[0, 0, 1], [1, 1, 1], [0, 1, 0]],
+                [[1, 0, 1], [1, 1, 1], [0, 1, 0]],
+                [[1, 1, 1], [1, 0, 1], [0, 1, 0]]])
     >>> format_converter(sample)
-    '1 2 5 5'
+    ['2 1 5 4', '1 2 5 4', '1 2 4 1 6 3']
     """
-    locations = []
-    counters = []
-    loc_count = []
+    final_string = []
+    for element in input_matrix_set:
+        
+        locations = []
+        counters = []
+        loc_count = []
 
-    array = np.transpose(input_matrix).flatten()
-    if array[0]==1:
-        locations.append(1)
-        counters.append(1)
+        array = np.transpose(element).flatten()
+        if array[0]==1:
+            locations.append(1)
+            counters.append(1)
 
-    for i in range(1,len(array)):
-        if array[i] == 1:
-            if array[i-1] == 0:
-                locations.append(i+1)
-                counters.append(1)
-            else:
-                counters[-1] += 1   
+        for i in range(1,len(array)):
+            if array[i] == 1:
+                if array[i-1] == 0:
+                    locations.append(i+1)
+                    counters.append(1)
+                else:
+                    counters[-1] += 1   
 
-    for i in range(len(locations)):
-        loc_count.append(locations[i])
-        loc_count.append(counters[i])
+        for i in range(len(locations)):
+            loc_count.append(locations[i])
+            loc_count.append(counters[i])
     
-    string = ' '.join(str(l) for l in loc_count)    
+        string = ' '.join(str(l) for l in loc_count)    
+        final_string.append(string)
     
-    return string
+    return final_string
 
 
 # Sample results
