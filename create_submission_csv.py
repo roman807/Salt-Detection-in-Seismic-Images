d9884574d8f8ce43229bf2964a3052c86f2c9af6
Amol Deshmukh <34318357+des137@users.noreply.github.com>
2018-10-07 21:20:26 -0400

    Function to create a string in required format

--- create_submission_csv.py
+++ create_submission_csv.py
@@ -8,6 +8,40 @@ $ ls -1 > list.txt
 
 import csv 
 
+def format_converter(input_matrix):
+    """
+    Converts a matrix into a string of the locations and counts of 1's: the format required by kaggle.
+    For e.g.
+    >>> sample = np.array([[1, 0, 1], [1, 1, 1], [0, 1, 1]])
+    >>> format_converter(sample)
+    '1 2 5 5'
+    """
+    locations = []
+    counters = []
+    loc_count = []
+
+    array = np.transpose(input_matrix).flatten()
+    if array[0]==1:
+        locations.append(1)
+        counters.append(1)
+
+    for i in range(1,len(array)):
+        if array[i] == 1:
+            if array[i-1] == 0:
+                locations.append(i+1)
+                counters.append(1)
+            else:
+                counters[-1] += 1   
+
+    for i in range(len(locations)):
+        loc_count.append(locations[i])
+        loc_count.append(counters[i])
+    
+    string = ' '.join(str(l) for l in loc_count)    
+    
+    return string
+
+
 # Sample results
 result = ['1 1']*18000
 
